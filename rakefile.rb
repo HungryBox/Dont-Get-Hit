c0d539a1dfabd170dbd045dc847ae5da2ea17091
George Gosieski <george.js.gosieski@gmail.com>
2015-06-26 11:06:48 -0700

    Continuing tests for windows suport, revised rake and gemfiles to prevent missing require errors

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,12 @@
+require "bundler"
+Bundler.setup
+
+require "rake"
+require "rspec"
+require "rspec/core/rake_task"
+
+
+
 task :default => [:build]
 
 task :build do
--- src/Game.rb
+++ src/Game.rb
@@ -3,3 +3,28 @@ require_relative './ScreenManager'
 
 window = ScreenManager.new()
 window.show
+
+
+Make sure to comment these classes in src_old
+
+# Bullet
+# CrediScreen
+# EnemyGen
+# PrimeWeapon
+# SideWinderEnemy
+# Button
+# Dev
+# GameScreen
+# Screen
+# SpacePlayer
+# Coin
+# DontGetHit
+# LevelScreen
+# ScreenManager
+# TitleScreen
+# Color
+# Enemy
+# OptionScreen
+# ShopScreen
+# ZOrder
+# Rakefile
